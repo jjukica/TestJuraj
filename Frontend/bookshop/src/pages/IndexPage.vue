a7bdf6d1879a83e3a7e732af8dea9498a40fe278
Juraj <jjukica@veleri.hr>
2024-12-05 10:59:22 +0100

    gotovo

---
+++ Frontend/bookshop/src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <div class="text-h2">
+        Juraj Jukica 5.12.2024.
+      </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # TestJuraj
 
 https://github.com/jjukica/TestJuraj
+
+SELECT COUNT(knjiga.autor) FROM knjiga JOIN autor ON knjiga.autor = autor.id WHERE autor.prezime_ime = "Andrić ivo"
+
